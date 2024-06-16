825e6b26bd88c85cb9fb63703c84be75f3adda9e
Ricardo A <ricardoamundaray1999@gmail.com>
2024-06-15 20:51:58 -0400

    eeoa

--- simoudo_project/simoudo_project/settings.py
+++ simoudo_project/simoudo_project/settings.py
@@ -162,7 +162,7 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
--- simoudo_project/simoudo_project/urls.py
+++ simoudo_project/simoudo_project/urls.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 handler404 = 'main_app.views.custom_404'
 
 urlpatterns = [
@@ -11,3 +15,5 @@ urlpatterns = [
      path('admin/', admin.site.urls),
     
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
