aaac65739e1fd5efe114aa3d9c5b88c0feef11bd
Ricardo A <ricardoamundaray1999@gmail.com>
2024-06-15 20:01:10 -0400

    ummmm

--- simoudo_project/simoudo_project/settings.py
+++ simoudo_project/simoudo_project/settings.py
@@ -167,7 +167,12 @@ if not DEBUG:    # Tell Django to copy static assets into a path called `staticf
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+    STORAGES = {
+
+            'staticfiles': {
+            'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+            },
+            }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
